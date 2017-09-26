dde6408d67664e55a899cab01a39d78b4106c450
Wilson Ndirangu <wndirangu@lastmile-networks.net>
2017-09-27 01:42:24 +0300

    add function to compute average

--- question_one.py
+++ question_one.py
@@ -49,3 +49,18 @@ class CsvManipulator(object):
         sortedlist =sorted(reader, key=lambda d: datetime.strptime(d['Harvesting date'], "%d/%m/%Y"), reverse=True)
 
         return sortedlist
+
+    def average_plants_harvest(self):
+        """
+        This function computes the average of plants harvest
+
+
+        Returns:
+            average : the average of the plants harvest
+            rtype: list
+        """
+        reader=self.read_csv()
+        no_of_plants_havest=[float(row['No of plants harvest']) for row in reader]
+        average=sum(no_of_plants_havest)/len(no_of_plants_havest)
+        return average
+
